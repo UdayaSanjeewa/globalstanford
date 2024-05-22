a3ba6f9dbcfeb3e9f1c13897dd11c6bdffb2d543
Thilina Sirinaga <thilina23city@gmail.com>
2024-05-23 01:50:17 +0530

    colors

--- src/components/BachelorDegrees/BachelorsDegreeHeading.tsx
+++ src/components/BachelorDegrees/BachelorsDegreeHeading.tsx
@@ -2,7 +2,7 @@ function BachelorsDegreeHeading(props: any) {
   return (
     //heading of Bachelors Degree
     <div className="w-full ">
-      <div className="h-[60px] w-full top-[208px] bg-[#000061] ">
+      <div className="h-[45px] w-full top-[208px] bg-[#003478] flex items-center justify-center">
         <h1 className="absolute left-[45px] sm:left-[100px] my-[14px] text-[22px] text-[white] font-mono leading-[1.3] tracking-wide font-[600]">
           {props.title}
         </h1>{" "}
--- src/components/MasterDegrees/MastersMainHeading.tsx
+++ src/components/MasterDegrees/MastersMainHeading.tsx
@@ -1,7 +1,7 @@
 function MastersMainHeading() {
   return (
     // Masters Degrees page Heading
-    <div className="flex items-center h-[85px] top-[208px] bg-[#000061] ">
+    <div className="flex items-center h-[45px] top-[208px] bg-[#003478]  justify-center ">
       <h1 className="absolute left-[45px] sm:left-[100px] my-[14px] text-[22px] text-[white] font-mono leading-[1.3] tracking-wide font-[600]">
         Master's Degrees
       </h1>
