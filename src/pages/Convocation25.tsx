91b1b0d63fe1a8ebc1b5912cae71e09c50e9bb78
P A Chathura Madhava <chathuratechdev@gmail.com>
2025-09-01 13:59:24 +0530

    feat: add footer

--- src/pages/Convocation25.tsx
+++ src/pages/Convocation25.tsx
@@ -3,10 +3,16 @@ import ProfileSection from "../components/ProfileSection";
 import MarketSection from "../components/MarketSection";
 import Gallery from "../components/Gallery";
 import Footer from "../components/Footer";
+import Navbar from "../components/Navbar";
+import SubNavbar from "../components/SubNavbar";
 
 function Convocation25() {
   return (
     <div className="min-h-screen bg-gray-50">
+      <div>
+        <Navbar />
+        <SubNavbar />
+      </div>
       <Hero />
       <ProfileSection />
       <MarketSection />
